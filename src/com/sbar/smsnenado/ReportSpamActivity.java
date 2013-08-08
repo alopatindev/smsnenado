6c053142c507cdcb6bfb12e56876521c0ebde639
Alexander Lopatin <sbar.geek@gmail.com>
2013-08-09 02:49:47 +0400

    GUI update

---
+++ src/com/sbar/smsnenado/ReportSpamActivity.java
@@ -0,0 +1,12 @@
+package com.sbar.smsnenado;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReportSpamActivity extends Activity {
+    @Override
+    public void onCreate(Bundle s) {
+        super.onCreate(s);
+        setContentView(R.layout.report_spam);
+    }
+}
