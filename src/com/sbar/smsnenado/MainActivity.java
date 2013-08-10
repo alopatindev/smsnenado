d7d5b329ad3b9f11a8cc1b0bdd5d4ff79d8dd50c
Alexander Lopatin <sbar.geek@gmail.com>
2013-08-10 22:36:07 +0400

    updated GUI

--- src/com/sbar/smsnenado/MainActivity.java
+++ src/com/sbar/smsnenado/MainActivity.java
@@ -136,7 +136,7 @@ public class MainActivity extends Activity {
         @Override
         public void onScroll(AbsListView view, int firstVisibleItem,
                 int visibleItemCount, int totalItemCount) {
-            if (firstVisibleItem + visibleItemCount + 2 >= totalItemCount)
+            if (firstVisibleItem + visibleItemCount >= totalItemCount)
                 MainActivity.this.updateSmsItemAdapter();
         }
 
