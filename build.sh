7a955805e8709fa0a14ba7d4d47015b13ae5a164
Alexander Lopatin <sbar.geek@gmail.com>
2013-09-22 20:12:20 +0400

    released 1.0.3

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ PROJECTNAME="smsnenado"
 PACKAGENAME="com.sbar.${PROJECTNAME}"
 
 SOURCE_ICON="assets/sms_spam_unsubscribed.png"
-SOURCE_CHECKBOX="art/checkbox_selected.png"
+#SOURCE_CHECKBOX="art/checkbox_selected.png"
 
 VERSION=$(egrep -o 'versionName="[0-9\.]*?"' AndroidManifest.xml |
           egrep -o '[0-9\.]*')
@@ -51,9 +51,9 @@ cp "$SOURCE_ICON" res/drawable-hdpi/ic_launcher.png
 convert "$SOURCE_ICON" -resize 48x48 res/drawable-mdpi/ic_launcher.png
 convert "$SOURCE_ICON" -resize 36x36 res/drawable-ldpi/ic_launcher.png
 
-convert "$SOURCE_CHECKBOX" -resize 72x72 res/drawable-hdpi/checkbox_selected.png
-convert "$SOURCE_CHECKBOX" -resize 48x48 res/drawable-mdpi/checkbox_selected.png
-convert "$SOURCE_CHECKBOX" -resize 36x36 res/drawable-ldpi/checkbox_selected.png
+#convert "$SOURCE_CHECKBOX" -resize 72x72 res/drawable-hdpi/checkbox_selected.png
+#convert "$SOURCE_CHECKBOX" -resize 48x48 res/drawable-mdpi/checkbox_selected.png
+#convert "$SOURCE_CHECKBOX" -resize 36x36 res/drawable-ldpi/checkbox_selected.png
 
 mkdir libs/
 ln -s $ANDROID_SDK/extras/android/support/v4/android-support-v4.jar libs/
